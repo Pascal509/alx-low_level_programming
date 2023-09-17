c87fc7a2f99260b0b1a2cb394814de9e50c46529
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2023-09-17 12:12:11 -0700

    first commit

---
+++ 0x02-functions_nested_loops/ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{	
+char ch = 'A';
+
+printf("the value of %c is %d\n", ch,ch);
+return (0);
+}
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,10 @@
 
 /**
   * mul - Multiplies two integers
-  * @x: The first number to multiply
-  * @y: The second number to multiply with first number
+  * @a The first number to multiply
+  * @b: The second number to multiply with first number
   *
-  * Return: The result of multiplication between x param and y param
+  * Return: The result of multiplication between a param and b param
   */
 int mul(int a, int b)
 {
