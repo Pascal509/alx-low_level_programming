e605a9d5fb430a0d13c8cac0cc2eb22f7f9cc521
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2023-09-28 13:06:33 -0700

    n

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+
+int main (void);
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+void _print_rev_recursion(char *s);
+
+int _strlen_recursion(char *s);
+
+int factorial(int n);
+
+int _pow_recursion(int x, int y);
+
+int find_root(int n, int i);
+
+int _sqrt_recursion(int n);
+
+int find_multipliers(int n, int i);
+
+int is_prime_number(int n);
+
+int _strlen_recursion(char *s);
+
+int pal_checker(char *s, int i, int j);
+
+int is_palindrome(char *s);
+
+
+#endif /*MAIN_H*/
