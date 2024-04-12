e10d2a74e58cb602993b0a20a92ccf6ba62cd7f5
Ezenagu Chinemerem <chinemeremezenagu@gmail.com>
2024-04-12 01:04:12 +0100

    first commit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include "search_algos.h"
 
-/* Write a function that searches for a value in an array
- * of integers using the Linear search algorithm */
+/**
+ * linear_search - Write a function that searches for a value in
+ * an array of integers using the Linear search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where the value is located, or -1 if not
+ * found or if array is NULL
+ */
+
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value in a
+ * sorted array of integers using the Binary search algorithm
+ *
+ * @array: pointer to the first element of array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: Index where the value is located, or -1 if not
+ * found or if array is NULL
+*/
+
 int binary_search(int *array, size_t size, int value)
 {
-	while (size <= value) 
-	{
+	size_t low = 0, high = size - 1, mid, i;
 
+	if (array == NULL)
+		return (-1);
+
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+
+		printf("Searching in array: ");
+		for (i = low; i <= high; i++)
+		{
+			printf("%d", array[i]);
+			if (i < high)
+				printf(", ");
+		}
+		printf("\n");
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
 	}
+	return (-1);
 }
